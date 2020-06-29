be42c09fba536e5675857970f9ed9725696089c9
Daniel Tucker <daniel@dantuck.com>
2020-06-29 06:48:58 -0600

    Added boilerplate for cli
    First part of cli includes commands for balance and register.

---
+++ src/cli.rs
@@ -0,0 +1,33 @@
+mod balance;
+mod register;
+
+use balance::*;
+use register::*;
+
+pub use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "bankroll")]
+pub struct Cli {
+    #[structopt(subcommand)]
+    pub cmd: Opt,
+}
+
+#[derive(Debug, StructOpt)]
+pub enum Opt {
+    #[structopt(name = "balance", aliases = &["b", "bal"])]
+    Balance(BalanceOpt),
+    #[structopt(name = "register", aliases = &["r", "reg"])]
+    Register(RegisterOpt),
+}
+
+pub fn run() -> Result<(), std::io::Error> {
+    let cli = Cli::from_args();
+
+    match &cli.cmd {
+        Opt::Balance(opt) => balance::eval(&cli, &opt)?,
+        Opt::Register(opt) => register::eval(&cli, &opt)?,
+    }
+
+    Ok(())
+}
